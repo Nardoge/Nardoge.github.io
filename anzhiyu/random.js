c5f4f96fade15fbaf5bc80d66820d17849cf0c03
huangjunwei <nardoge@163.com>
2024-04-30 08:26:59 +0800

    Site updated: 2024-04-30 08:26:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/23/人生就是要不断开始/","2024/04/26/雨天总是挂念从前/"];function toRandomPost(){
+var posts=["2024/04/23/人生就是要不断开始/","2024/04/26/雨天总是挂念从前/","2024/04/30/RSA加密/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
